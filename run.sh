18b56fb4ed53e9393852460155bec927b4086487
yulahuyed <yulahuyed@vpsorg.top>
2018-02-22 18:34:18 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [ ! -f "/root/aria2/aria2.session" ]; then
+  touch /root/aria2/aria2.session
+fi
+
+if [ ! -f "/root/aria2/dht.dat" ]; then
+  touch /root/aria2/dht.dat
+fi
+
+# Enable for IPV6 DHT and Edit aria2.conf
+#if [ ! -f "/root/aria2/dht6.dat" ]; then
+#  touch /root/aria2/dht.dat
+#fi
+
+if [ "${RCLONE_CONFIG}" ]
+then
+wget -O ~/.config/rclone/rclone.conf ${RCLONE_CONFIG}
+fi
+
+/usr/bin/aria2c --conf-path /root/aria2/aria2.conf

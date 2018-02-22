810f848f577a5728203119fe6f36d48d4ad61119
yulahuyed <yulahuyed@vpsorg.top>
2018-02-22 20:01:46 +0800

    Create pdown.sh

---
+++ pdown.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+slack () {
+  if [ "${SLACK}" ]
+  then
+  curl -X POST -H 'Content-type: application/json' --data '{"text": "$1", "channel": "#private", "link_names": 1, "username": "Aria2", "icon_emoji": ":monkey_face:"}' ${SLACK}
+  fi
+} 
+
+echo "$0 $1 $2 $3" >> /root/aria2/errdown.log
+
+slack "The download have stopped.\n$3"

debdd32e971686a608cf63ce7c9a07bf7fa42ed3
yulahuyed <yulahuyed@vpsorg.top>
2018-02-23 22:45:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,8 +17,7 @@ fi
 
 if [ "${RCL_LINK}" ]
 then
-mkdir -p ~/.config/rclone
-curl -L -o ~/.config/rclone/rclone.conf "${RCL_LINK}" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0"
+curl -L -o /aria2/rclone.conf "${RCL_LINK}" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0"
 fi
 
 

baddab43daf659567e3d82e9ee0d5e084e2c13d8
yulahuyed <yulahuyed@vpsorg.top>
2018-02-23 10:18:37 +0800

    Update sdown.sh

--- sdown.sh
+++ sdown.sh
@@ -22,18 +22,18 @@ else
   RCLONE_TYPE=$(cat ~/.config/rclone/rclone.conf | grep -oE '\[.*\]' | sed 's/\[\(.*\)\]/\1/g')
 fi
 
-DLPATH="/root/downloads"
-FILEPATH=`echo "$3" | sed "s#$DLPATH/\(.*\)/.*#\1#"`
-
 slack "The files have downloaded, and start to upload.\n$3"
 
-if [ -f "$DLPATH/$FILEPATH" ]
+DLPATH="/root/downloads"
+
+if [[ $3 =~ $DLPATH/.*/.* ]]
 then
-rclone copy $3 $RCLONE_TYPE:
-rm -f $3
-else
+FILEPATH=`echo "$3" | sed "s#$DLPATH/\(.*\)/.*#\1#"`
 rclone copy $DLPATH/$FILEPATH $RCLONE_TYPE:$FILEPATH
 rm -rf $DLPATH/$FILEPATH
+else
+rclone copy $3 $RCLONE_TYPE:
+rm -f $3
 fi
 
 slack "The files have downloaded and uploaded.\n$3"

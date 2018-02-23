845851136cba1d05616e0e1f37825937620461cc
yulahuyed <yulahuyed@vpsorg.top>
2018-02-23 11:31:59 +0800

    Update sdown.sh

--- sdown.sh
+++ sdown.sh
@@ -3,7 +3,7 @@
 slack () {
   if [ "${SLACK}" ]
   then
-  curl -X POST -H 'Content-type: application/json' --data '{"text": "$1", "channel": "#private", "link_names": 1, "username": "Aria2", "icon_emoji": ":monkey_face:"}' ${SLACK}
+  curl -X POST -H "Content-type: application/json" --data "{\"text\": \"$1\", \"channel\": \"#private\", \"link_names\": 1, \"username\": \"Aria2\", \"icon_emoji\": \":monkey_face:\"}" ${SLACK}
   fi
 } 
 
